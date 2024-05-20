feb4dd8fb450b988069ab53a465038e11b7d5f5d
Lee Walters <lee@leewalters.net>
2024-05-20 14:08:56 +0100

    Target the de-origin feature deployment to test outside of network

--- build-utils/webpack.development.js
+++ build-utils/webpack.development.js
@@ -12,7 +12,9 @@ var properties = {
     de_common_ui:
       "de_common_ui@https://dev.originaec.app/common/remoteEntry.js",
     de_origin:
-      "de_origin@https://dev.originaec.app/remoteEntry.js",
+      // "de_origin@https://dev.originaec.app/remoteEntry.js",
+      "de_origin@https://origin-tf-dev-web-chore-12341.azurewebsites.net//remoteEntry.js",
+      // "de_origin@http://localhost:3001/remoteEntry.js",
   },
 };
 
